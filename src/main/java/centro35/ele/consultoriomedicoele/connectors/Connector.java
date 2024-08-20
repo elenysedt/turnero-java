035ba91ab23de0360cb7c16533d18f495809bf9a
elenysedt <elenysedt@gmail.com>
2024-08-19 23:22:11 -0300

    integration db postgre

--- src/main/java/centro35/ele/consultoriomedicoele/connectors/Connector.java
+++ src/main/java/centro35/ele/consultoriomedicoele/connectors/Connector.java
@@ -1,12 +1,11 @@
 package centro35.ele.consultoriomedicoele.connectors;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Connector {
-
-    // SQlite
-    private static String url = "jdbc:sqlite:./data/dataele.db";
 
     private static Connection conn = null;
 
@@ -15,15 +14,34 @@ public class Connector {
 
     public static Connection getConnection() {
         try {
-            conn = DriverManager.getConnection(url);
-        } catch (Exception e) {
-            System.out.println(e);
+            if (conn == null || conn.isClosed()) {
+                // Cargar las variables de entorno desde el archivo .env si está presente
+                Dotenv dotenv = Dotenv.load();
+                
+                // Obtener las variables de entorno, usando valores por defecto si es necesario
+                String url = System.getenv("DB_URL");
+                String user = System.getenv("DB_USER");
+                String password = System.getenv("DB_PASSWORD");
+                
+                if (url == null) url = dotenv.get("DB_URL");
+                if (user == null) user = dotenv.get("DB_USER");
+                if (password == null) password = dotenv.get("DB_PASSWORD");
+
+                // Imprimir los valores para depuración
+                //System.out.println("Database URL: " + url);
+                //System.out.println("Database User: " + user);
+                //System.out.println("Database Password: " + password);
+            
+                // Cargar el driver JDBC
+                conn = DriverManager.getConnection(url, user, password);
+            }
+        } catch (SQLException e) {
+            System.out.println("Connection failed: " + e.getMessage());
         }
         return conn;
     }
 
     public static String getUrl() {
-        return url;
+        return System.getenv("DB_URL");
     }
-
 }
